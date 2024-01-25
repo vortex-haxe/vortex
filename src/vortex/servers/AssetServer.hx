5f095f7b5950f5a3b79f72a95a3d897c71997baf
swordcube <swordsoftarchive@gmail.com>
2024-01-24 21:31:08 -0600

    uhhhhhhhhhhh

---
+++ src/vortex/servers/AssetServer.hx
@@ -0,0 +1,18 @@
+package vortex.servers;
+
+import vortex.backend.interfaces.IServer;
+
+class AssetServer extends IServer {
+	/**
+	 * Initializes this asset server.
+	 */
+	public static function init():Void {}
+
+	/**
+	 * Disposes of this rendering server and removes it's
+	 * properties from memory.
+	 */
+	public static function dispose():Void {
+
+	}
+}
